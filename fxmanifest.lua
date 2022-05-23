be4c15c1b0ebcb6425a69e7d9ae583417a9bdb33
Panda <90091858+PandaSLO@users.noreply.github.com>
2022-05-23 19:57:56 +0200

    refactor(fxmanifest): Fixed the FXManifest

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+ui_page 'html/scoreboard.html'
+
+files {
+  'html/scoreboard.html',
+  'html/listener.js',
+  'html/style.css'
+
+client_scripts {
+  'client/main.lua'
+}
+
+server_scripts {
+  'main.lua'
+}
+
