54921464f2710570ee22e511cd621f97979ada7a
Sadra Khorami <30285010+SadraKhorami@users.noreply.github.com>
2022-05-23 11:02:44 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+ui_page 'html/scoreboard.html'
+
+files {
+  'html/*',
+
+client_scripts {
+  'client/main.lua'
+}
+
+server_scripts {
+  'main.lua'
+}
